22b111eaecd8e0e098465eba6515724676d22202
unknown <C4r105>
2023-01-18 09:17:22 -0700

    v1.0

---
+++ main.js
@@ -0,0 +1,18 @@
+addEventListener('DOMContentLoaded', ()=> {
+    contador();
+});
+
+let contador = ()=> {
+    let clicks = 0;
+    let boton = document.getElementById('boton');
+    boton.addEventListener('click', ()=> {
+        clicks++;
+        boton.innerHTML = clicks;
+        boton.style.backgroundColor = `rgb(${numeroRandom(255, 150)}, ${numeroRandom(255,150)}, ${numeroRandom(255,150)})`;
+    });
+}
+
+let numeroRandom = (max, min)=> {
+    numero = Math.random()*(max - min) + min;
+    return numero;
+}
